ad8ef6f3495bc1f88df3541a1f33807519bcbbe6
BuildTools <unconfigured@null.spigotmc.org>
2021-12-05 20:28:26 -0500

    added hitpoint and ability score calculation

---
+++ calculations.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def hitpoints(ch_class, ch_level, constitution):
+    #D6
+    if(ch_class == "Sorcerer" or ch_class == "Wizard"):
+        if (ch_level == 1):
+            return abilityscore(constitution) + 6 + constitution
+        else:
+            return abilityscore(constitution) + ((4 + constitution) * ch_level)
+    #D8
+    elif(ch_class == "Bard" or ch_class == "Cleric" or ch_class == "Druid" or ch_class == "Monk" or ch_class == "Rogue" or ch_class == "Warlock"):
+        if (ch_level == 1):
+            return abilityscore(constitution) + 8 + constitution
+        return abilityscore(constitution) + ((5 + constitution) * ch_level)
+    #D10
+    elif(ch_class == "Fighter" or ch_class == "Paladin" or ch_class == "Ranger"):
+        if (ch_level == 1):
+            return abilityscore(constitution) + 10 + constitution
+        return abilityscore(constitution) + ((6 + constitution) * ch_level)
+    #D12
+    elif (ch_class == "Barbarian"):
+        if (ch_level == 1):
+            return abilityscore(constitution) + 12 + constitution
+        else:
+            return abilityscore(constitution) + ((7 + constitution) * ch_level)
+    else:
+        return 0
+
+def abilityscore(stat):
+    ret = -5
+    for x in range(22):
+        if (x % 2) == 0:
+            ret += 1
+    return ret
